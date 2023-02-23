5902714f848dd8d53a4c4bfbb78a27def0c992a5
Bibek Raj Shrestha <Bibekrajshrestha@gmail.com>
2023-02-23 10:48:47 +0545

    Add files via upload

---
+++ temp_functions.py
@@ -0,0 +1,15 @@
+#defining a function to convert fahr to celsius
+def fahr_to_celsius (temp_fahrenheit):
+    return(temp_fahrenheit - 32)/1.8
+
+def temp_classifier(temp_celsius):
+    if (temp_celsius<-2):
+        return 0
+    if (temp_celsius>=-2 and temp_celsius<2):
+        return 1
+    if (temp_celsius>=2 and temp_celsius<15):
+        return 2
+    if (temp_celsius>=15):
+        return 3
+    
+   
